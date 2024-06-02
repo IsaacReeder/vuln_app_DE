dc9d147e91c1705c4106a9e487af621f59ca39ac
isaacreeder <isaacreeder5@gmail.com>
2024-06-02 17:25:53 +0200

    XSS

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        email = request.form['email']  # This input should be sanitized but is not for this example
+        return render_template('signup_success.html', email=email)
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])  # Add route decorator for signup page
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']  # This input should be sanitized but is not for this example
+        return render_template('signup_success.html', email=email)
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
